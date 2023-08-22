af02875ffe7b177dd2e289bf8dfa964290325b5d
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-08-22 11:23:06 -0500

    Find Files Rpc

--- api_gateway/main.js
+++ api_gateway/main.js
@@ -7,13 +7,18 @@ const app = express();
 const PORT = 3000;
 
 // file service 
-const PROTO_PATH = path.resolve(__dirname, '../proto/list_files.proto');
+const listFilePROTO_PATH = path.resolve(__dirname, '../proto/list_files.proto');
 const listFilePORT = 50051;
-const packageDefinition = protoLoader.loadSync(PROTO_PATH);
-const listFileService = grpc.loadPackageDefinition(packageDefinition).FileService;
+const listFilePackageDefinition = protoLoader.loadSync(listFilePROTO_PATH);
+const listFileService = grpc.loadPackageDefinition(listFilePackageDefinition).FileService;
 const listFileClient = new listFileService(`127.0.0.1:${listFilePORT}`,grpc.credentials.createInsecure());
 
 // search service
+const findFilePROTO_PATH = path.resolve(__dirname, '../proto/find_file.proto');
+const findFilePORT = 50052;
+const findFilePackageDefinition = protoLoader.loadSync(findFilePROTO_PATH);
+const findFileService = grpc.loadPackageDefinition(findFilePackageDefinition).FileService;
+const findFileClient = new findFileService(`127.0.0.1:${findFilePORT}`,grpc.credentials.createInsecure());
 
 // list all files endpoint
 app.get('/list', (req, res) => {
@@ -29,7 +34,14 @@ app.get('/list', (req, res) => {
 
 // search file 
 app.get('/search', (req, res) => {
-  res.send('search');
+  findFileClient.FindFile({ name: req.query.name }, (error, response) => {
+    if (error) {
+      console.error('Error calling the gRPC service', error);
+      res.status(500).send('Error calling microservice');
+      return;
+    }
+    res.json(response);
+  });
 });
 
 app.listen(PORT, () => {
--- find_file/main.js
+++ find_file/main.js
@@ -3,29 +3,30 @@ const protoLoader = require('@grpc/proto-loader');
 const path = require('path');
 const fs = require('fs');
 
-const PROTO_PATH = path.resolve(__dirname, '../proto/list_files.proto');
+const PROTO_PATH = path.resolve(__dirname, '../proto/find_file.proto');
 const packageDefinition = protoLoader.loadSync(PROTO_PATH);
 const fileServiceProto = grpc.loadPackageDefinition(packageDefinition);
 
 const DIRECTORY_PATH = path.join(__dirname, '../files');
-const PORT = 50051;
+const PORT = 50052;
 
 const server = new grpc.Server();
 
-const listFiles = (call, callback) => {
-    fs.readdir(DIRECTORY_PATH, (err, files) => {
-        if (err) {
-            callback({
-                code: grpc.status.INTERNAL,
-                details: "Error reading directory"
-            });
-            return;
-        }
-        callback(null, { filenames: files });
-    });
+const exists = (call, callback) => {
+  fs.readdir(DIRECTORY_PATH, (err, files) => {
+    if (err) {
+      callback({
+          code: grpc.status.INTERNAL,
+          details: "Error reading directory"
+      });
+      return;
+  }
+    const fileExist = files.includes(call.request.name);
+    callback(null, { existence: fileExist });
+});
 };
 
-server.addService(fileServiceProto.FileService.service, { ListFiles: listFiles });
+server.addService(fileServiceProto.FileService.service, { FindFile: exists });
 server.bindAsync(`127.0.0.1:${PORT}`, grpc.ServerCredentials.createInsecure(), () => {
     server.start();
     console.log(`gRPC server running on http://127.0.0.1:${PORT}`);
