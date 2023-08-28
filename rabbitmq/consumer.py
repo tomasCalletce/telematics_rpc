2ba45776147ce4513b46a09b9d3571488c3e6c90
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-08-27 21:23:56 -0500

    add list to consumer

--- rabbitmq/consumer.py
+++ rabbitmq/consumer.py
@@ -1,10 +1,24 @@
 import pika
+import os
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('52.55.99.177', 5672, '/', pika.PlainCredentials("user", "password")))
 channel = connection.channel()
 
+def list_files(directory):
+    with os.scandir(directory) as entries:
+        return [entry.name for entry in entries if entry.is_file()]
+
 def callback(ch, method, properties, body):
-    print(f'{body} is received')
+    if(body == b'list'):
+        directory_path = "../files"
+        files = list_files(directory_path)
+        for file_name in files:
+          print(file_name)
+        return
+    if(body == b'help'):
+        print('help')
+        return
+    channel.stop_consuming()
     
 channel.basic_consume(queue="serve-queue", on_message_callback=callback, auto_ack=True)
 channel.start_consuming()
